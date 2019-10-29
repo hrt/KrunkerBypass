affc1edea09e69b4c135b72781adfcd292b88d5c
Inusha <inusha@outlook.com>
2019-10-29 18:39:02 +0000

    Update esp.js

--- DecoderHook/esp.js
+++ DecoderHook/esp.js
@@ -15,10 +15,14 @@
 	    	var nodes = mutations[i].addedNodes;
 	    	for (var j = 0; j < nodes.length; j++) {
 	    		var node = nodes[j];
-	    		if (node.tagName == 'SCRIPT' && node.nodeName == 'SCRIPT' && node.src == '' && node.innerHTML.includes('Sidney')) {
+	    		if (node.tagName == 'SCRIPT' && node.nodeName == 'SCRIPT' && node.src == '') {
 					var decoderRegex = /return (\w+\.decode\(\w+\(\)\.subarray\(\w+,\w+\+\w+\)\))/;
-					var decoder = node.innerHTML.match(decoderRegex)[1];
-	    			node.innerHTML = node.innerHTML.replace(decoderRegex, 'var script=' + decoder + `;script=script.replace(/if\\(!tmpObj\\['inView'\\]\\)continue;/, '');return script`);
+					var decoder = node.innerHTML.match(decoderRegex);
+					if (!decoder) {
+						continue;
+					}
+					decoder = decoder[1];
+	    			node.innerHTML = node.innerHTML.replace(decoderRegex, 'var script=' + decoder + `;script=script.replace(/if\\(!\\w+\\['\\w+Seen'\\]\\)continue;/, '');return script`);
 	    		}
 	    	}
 	    }
