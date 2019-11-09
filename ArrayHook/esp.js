53fbd84e9565a4c9b696e040d6c60776e39e7352
Inusha <inusha@outlook.com>
2019-11-09 11:26:56 +0000

    Update esp.js

--- ArrayHook/esp.js
+++ ArrayHook/esp.js
@@ -10,13 +10,27 @@
 // ==/UserScript==
 
 (function(){
-    var hideHook = function(fn, oFn) { fn.toString = oFn.toString.bind(oFn); }
     var original_push = Array.prototype.push;
-    Array.prototype.push = function(...args) {
+    function hook_push(...args) {
         original_push.apply(this, args);
         if (args[0] && args[0].armMeshes) {
             Object.defineProperty(args[0], "lhYWIWew", { get: function() { return true } });
         }
     }
-    hideHook(Array.prototype.push, original_push);
+
+    var string_map = {};
+	var original_toString = Function.prototype.toString;
+	function hook_toString(...args) {
+		var return_string = original_toString.apply(this, args);
+		if (string_map[return_string]) {
+			return_string = string_map[return_string];
+		}
+		return return_string;
+	}
+
+	string_map[hook_push.toString()] = original_push.toString();
+	string_map[hook_toString.toString()] = original_toString.toString();
+
+    Array.prototype.push = hook_push;
+	Function.prototype.toString = hook_toString;
 })()
