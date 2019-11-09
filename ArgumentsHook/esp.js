da70ae9e2396076da28f709e289401c955035a6b
Inusha <inusha@outlook.com>
2019-11-09 11:30:41 +0000

    Update esp.js

--- ArgumentsHook/esp.js
+++ ArgumentsHook/esp.js
@@ -16,9 +16,6 @@
 */
 
 (function(){
-	var original_clearRect = CanvasRenderingContext2D.prototype.clearRect;
-
-	var hideHook = function(fn, oFn) { fn.toString = oFn.toString.bind(oFn); }
 
 	var find_player_list = function(root) {
 		var current = root;
@@ -37,20 +34,32 @@
 	var bypass_use_strict = function() {
 		// now we can use arguments, callee, caller
 		var players = find_player_list(arguments.callee);
-		console.log(players);
 		for (var i = 0; players && i < players.length; i++) {
 			players[i]['lhYWIWew'] = true;
 		}
 	}
 
-	var rigged_clearRect = function(...args) {
+	var original_clearRect = CanvasRenderingContext2D.prototype.clearRect;
+	function hook_clearRect(...args) {
 		original_clearRect.apply(this, args);
 		// we cannot access arguments, callee, caller here due to 'use strict'
 		// so instead call a function which isn't bounded by 'use strict'
 		bypass_use_strict();
 	}
 
-	hideHook(rigged_clearRect, original_clearRect)
-	CanvasRenderingContext2D.prototype.clearRect = rigged_clearRect;
-	console.log(CanvasRenderingContext2D.prototype.clearRect);
+    var string_map = {};
+	var original_toString = Function.prototype.toString;
+	function hook_toString(...args) {
+		var return_string = original_toString.apply(this, args);
+		if (string_map[return_string]) {
+			return_string = string_map[return_string];
+		}
+		return return_string;
+	}
+
+	string_map[hook_clearRect.toString()] = original_clearRect.toString();
+	string_map[hook_toString.toString()] = original_toString.toString();
+
+	CanvasRenderingContext2D.prototype.clearRect = hook_clearRect;
+	Function.prototype.toString = hook_toString;
 })()
